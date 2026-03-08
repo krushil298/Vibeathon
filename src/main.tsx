e4f634852935efa85671fafbb6e9cb02fb2c09ed
Uchadadia Krushil Mukeshbhai <krushiluchadadia@gmail.com>
2026-03-09 03:10:23 +0530

    chore: configure github pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Vibeathon">
       <App />
     </BrowserRouter>
   </StrictMode>,
