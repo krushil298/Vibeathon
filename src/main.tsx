85310de97ad9b706546149cd0c51c0b0c64f3083
Uchadadia Krushil Mukeshbhai <krushiluchadadia@gmail.com>
2026-03-09 03:14:54 +0530

    chore: remove gh-pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Vibeathon">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
